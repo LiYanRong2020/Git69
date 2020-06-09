9b0be152a1a2f85010db4ad7d6a9f167feb68d00
LiYanRong2020 <2267765378@qq.com>
2020-06-09 17:14:08 +0800

    把Git-Project提交到远程仓库

---
+++ Git-Project/src/com/git/li/Git.java
@@ -0,0 +1,23 @@
+package com.git.li;
+
+import java.util.List;
+
+public class Git {
+
+	
+	public void adduser(int id,String username,String newPasword) {
+		
+	}
+	
+	public void deleteUser(int id) {
+		
+	}
+	
+	public void updateUser(int id,String newPassword) {
+		
+	}
+	
+	public  List<Object> scanUser (int pageIndex,int pageSize){
+		return null;
+	}
+}
